70f7f9e1a9f376c4e8a3e96c7c2b02bc790c930b
DavidSalazarG <david.1701713821@ucaldas.edu.co>
2023-10-15 22:05:24 -0500

    Se modifica el frontend para mostrar los productos en Cards y mostrar la imagen del producto, tambien se arregla el error de crasheo del navegador

--- frontend/lib/scrapping.dart
+++ frontend/lib/scrapping.dart
@@ -34,19 +34,15 @@ class _ScrapeProductsWidgetState extends State<ScrapeProductsWidget> {
     });
 
     final searchTerm = _searchTermController.text;
-    //En caso de querer hacerlo móvil, poner la dirección: http://10.0.2.2:8000/scrape_aliexpress/
     final apiUrl = Uri.parse('http://127.0.0.1:8000/scrape_both/');
 
     final response = await http.post(
       apiUrl,
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-      }, // Especifica la codificación UTF-8
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
       body: jsonEncode({'search_term': searchTerm}),
     );
 
     if (response.statusCode == 200) {
-      // Decodifica la respuesta utilizando la codificación UTF-8
       final productData =
           jsonDecode(utf8.decode(response.bodyBytes))['productos'];
       setState(() {
@@ -62,7 +58,6 @@ class _ScrapeProductsWidgetState extends State<ScrapeProductsWidget> {
     }
   }
 
-  // Función para abrir el enlace en el navegador
   Future<void> _openLink(String link) async {
     String encodedLink = Uri.encodeFull(link);
     final url = Uri.parse(encodedLink);
@@ -71,6 +66,24 @@ class _ScrapeProductsWidgetState extends State<ScrapeProductsWidget> {
       await launchUrl(url);
     } else {
       throw 'No se pudo abrir el enlace $link';
+    }
+  }
+
+  Widget _buildProductImage(String productImage) {
+    try {
+      return Image.network(
+        productImage,
+        width: 100,
+        height: 100,
+        fit: BoxFit.cover,
+        errorBuilder: (context, error, stackTrace) {
+          return Icon(
+              Icons.error); // Puedes cambiar esto a una imagen de reemplazo
+        },
+      );
+    } catch (e) {
+      print('Error al cargar la imagen: $e');
+      return Icon(Icons.error); // Devuelve un icono de error en caso de error
     }
   }
 
@@ -98,16 +111,42 @@ class _ScrapeProductsWidgetState extends State<ScrapeProductsWidget> {
                 itemCount: _productData.length,
                 itemBuilder: (context, index) {
                   var productLink = _productData[index]['Link'];
-                  return InkWell(
-                    // Usamos InkWell para detectar el toque
-                    onTap: () => _openLink(productLink), // Abrir el enlace
-                    child: ListTile(
-                        title: Text(
-                            'Producto : ${_productData[index]['Product']}'),
-                        subtitle: Text(
-                            'Precio en COP: ${_productData[index]['Price']}'),
-                        trailing: Text(
-                            'Comprar en: ${obtenerPagina(_productData[index]['Link'])}')),
+                  var productImage = _productData[index]['Image'];
+
+                  return Card(
+                    child: InkWell(
+                      onTap: () => _openLink(productLink),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: [
+                            _buildProductImage(productImage),
+                            SizedBox(
+                                width: 16.0), // Espacio entre imagen y texto
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Producto: ${_productData[index]['Product']}',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Precio en COP: ${_productData[index]['Price']}',
+                                  ),
+                                  Text(
+                                    'Comprar en: ${obtenerPagina(_productData[index]['Link'])}',
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   );
                 },
               ),
@@ -117,12 +156,12 @@ class _ScrapeProductsWidgetState extends State<ScrapeProductsWidget> {
       ),
     );
   }
-}
 
-obtenerPagina(String link) {
-  if (link.contains("alibaba")) {
-    return "Alibaba";
-  } else if (link.contains("aliexpress")) {
-    return "Aliexpress";
+  obtenerPagina(String link) {
+    if (link.contains("alibaba")) {
+      return "Alibaba";
+    } else if (link.contains("aliexpress")) {
+      return "Aliexpress";
+    }
   }
 }
